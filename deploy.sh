1204b65df94a16d58d1aae7a1de4bb83e28965ec
Vignesh05 <135207642+Vigneshvicky05@users.noreply.github.com>
2025-02-05 16:21:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u vigneshwaran1806 -p Vignesh#2005
-    docker tag test vigneshwaran1806/task2
-    docker push vigneshwaran1806/task2
+    docker tag test vigneshwaran1806/task3
+    docker push vigneshwaran1806/task3
     
